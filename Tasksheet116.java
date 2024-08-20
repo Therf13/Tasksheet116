fe16827930ed688489384edbfc990ec338c3fde3
Therf13 <157944898+Therf13@users.noreply.github.com>
2024-08-20 13:18:45 +0000

    Finish

--- Tasksheet116.java
+++ Tasksheet116.java
@@ -1,1 +1,20 @@
+class Tasksheet116 {
+    public static void main(String[] args) {
+
+        StringBuilder str = new StringBuilder("Live");
+
+        StringBuilder reverseStr = str.reverse();
+
+        if (str.equals(reverseStr)); {
+            System.out.println("The input string is a palindrome");
+        }
+
+        if (reverseStr.equals(str)); {
+            System.out.println("The input string is not a palindrome");
+        }
+    }
+}
+
+
+
 
